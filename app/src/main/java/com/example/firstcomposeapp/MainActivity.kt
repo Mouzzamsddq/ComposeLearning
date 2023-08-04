618b642e43118440e77c4d62507587a0081b03ef
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-08-04 17:13:47 +0530

    learn modifiers basic

--- app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +29,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier.fillMaxSize()
-                    .background(Color.Green),
+                    .background(Color.Green)
+                    /**
+                     * diff b/w width and required width attribute
+                     * If we are passing width less than maximum parent width, it will use the given width. If we are passing width exceeding the parent maximum width,
+                     * then it will consider the parent maximum width.
+                     * But in case of requiredWidth attribute, it will always consider the given width whether it exceeds with the parent maximum width. it does not care.
+                     */
+                    .width(300.dp),
+//                    .requiredWidth(600.dp),
             ) {
                 Column(
-                    modifier = Modifier.fillMaxHeight(0.5f).background(Color.Red).fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceAround,
+                    /**
+                     * In the jetpack compose, modifier execute sequentially, so we can call same method one after another, it will lead
+                     * to the different result.
+                     */
+                    modifier = Modifier
+                        .fillMaxHeight(0.5f)
+                        .background(Color.Red)
+                        .fillMaxWidth()
+                        .border(width = 5.dp, color = Color.Black)
+                        .padding(20.dp)
+                        .border(width = 5.dp, color = Color.Blue)
+                        .padding(20.dp),
                 ) {
-                    Text(text = "Hello")
+                    Text(
+                        text = "Hello",
+                        /**
+                         * With the help of modifier we can make it intractable also
+                         */
+                        modifier = Modifier.clickable {
+                        },
+                    )
+                    Spacer(modifier = Modifier.height(30.dp))
                     Text(text = "World")
                     Text(text = "Hello")
                     Text(text = "Hello")
