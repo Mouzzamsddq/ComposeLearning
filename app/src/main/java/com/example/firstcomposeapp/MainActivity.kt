0d77916116da5eac07d2a260af60b423d618b2b8
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-08-04 16:00:40 +0530

    completed row and column feature

--- app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
@@ -3,49 +3,48 @@ package com.example.firstcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcomposeapp.ui.theme.FirstComposeAppTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .background(Color.Green),
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxHeight(0.5f).background(Color.Red).fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround,
                 ) {
-                    Greeting("Mouzzam")
+                    Text(text = "Hello")
+                    Text(text = "World")
+                    Text(text = "Hello")
+                    Text(text = "Hello")
+                    Text(text = "Hello")
+                }
+                Row(
+                    modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Blue),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "World")
+                    Text(text = "Hello")
+                    Text(text = "Hello")
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstComposeAppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background,
-        ) {
-            Greeting("Anjhgjhhgjhgdroid")
         }
     }
 }
